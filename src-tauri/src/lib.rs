e0f68ef418bea6cd640ab392613bceeb33df1249
Ishan Sahu <ishan20015.s@gmail.com>
2026-08-11 04:11:08 +0000

    fix: open files with OS default handlers

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,7 +15,7 @@ mod updater;
 use disks::get_disk_topology;
 use fs::{
     cancel_operation, copy_items, create_file, create_folder, delete_items, get_file_preview, get_path_metadata,
-    get_quick_access, list_directory, list_graph_children, move_items, rename_item, search_directory,
+    get_quick_access, list_directory, list_graph_children, move_items, open_file_with_default, rename_item, search_directory,
 };
 use helpers::settings::{get_settings, load_settings, set_settings};
 use network::{fetch_wallpaper_image, get_cached_wallpaper_image, get_wallpaper, get_wallpaper_updated_at, search_wallpapers};
@@ -113,6 +113,7 @@ pub fn run() {
             get_disk_topology,
             get_folder_size,
             recompute_folder_size,
+            open_file_with_default,
             cancel_operation,
             get_settings,
             set_settings,
